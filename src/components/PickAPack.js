cd787b5be9d49f2596348fcb6f9d39399e705ea9
Michael <michaelodell660@gmail.com>
2021-03-27 10:17:44 -0700

    added ability to create new pack inside pick a pack component

--- src/components/PickAPack.js
+++ src/components/PickAPack.js
@@ -2,11 +2,15 @@ import React from 'react';
 import{ useEffect, useState, useRef} from 'react';
 import Pack from './Pack';
 
+
 const PickAPack = () => {
 
     const [packList, setPackList] = useState([]);
     const [packstate, setPackState] = useState('');
+
     const activePack = useRef(null);
+    const packName = useRef(null);
+
     useEffect(()=>{
         ( async ()=>{
             try{
@@ -19,13 +23,32 @@ const PickAPack = () => {
         })()
     },[]
     )
+
     const viewPack = (e) => {
         e.preventDefault();
         setPackState([...activePack.current.value]);
     }
-    // const printTest = () =>{
-    //     console.log(activePack.current.value)
-    // }
+
+    const createPack = async e =>{
+        e.preventDefault();
+        try{
+            const response = await fetch('http://localhost:3001/packs/', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name: packName.current.value
+                })
+            })
+            const data = await response.json();
+            setPackList([...packList, data]);
+        }catch(error){
+            console.error(error)
+        }
+    }
+
+    
     return(
         <div className="pack-choice">
                   {/* ///////////////DROP DOWN/////////////////// */}
@@ -39,7 +62,14 @@ const PickAPack = () => {
         </select>
             <input type='submit' value="view pack"/>
         </form>
+        <div className='newpack-module'>
 
+            <form onSubmit={createPack}>
+                Pack Name: <input ref={packName} type="string"/>
+                <input type='submit' value='create pack'/>
+            </form>
+        </div>
+            
             {activePack.current? <Pack packId={packstate}/> : ""}
         </div>
     )
