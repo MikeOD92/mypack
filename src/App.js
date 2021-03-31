c4eb323203809fb219ab5155b70a13f1c6de0ed7
Michael <michaelodell660@gmail.com>
2021-03-31 14:28:18 -0700

    deleted unused react files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import React from 'react';
-import PickAPack from './components/PickAPack';
 import Selector from './components/Selector';
 function App() {
   
   return (
     <div className="App">
-      {/* <PickAPack/> */}
           <Selector/>
     </div>
   );
